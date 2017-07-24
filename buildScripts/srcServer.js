7c94ad7743d0d88af5fe86693f5688d7151f98dc
Jon Love <jonlov4@gmail.com>
2017-07-23 23:24:30 -0400

    dynamic and random mock data and api for testing and prototyping

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -16,13 +16,21 @@ app.use(require('webpack-dev-middleware')(compiler, {
 }));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.get('/users', (req, res) => {
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Lee","email":"tammy@gmail.com"},
+    {"id": 3,"firstName":"Jim","lastName":"Halpert","email":"jim@dundermifflin.com"}
+  ]);
 });
 
 app.listen(port, (err) => {
-    if (err) {
-        console.log(err);
-    }else {
-        open('http://localhost:' + port);
-    }
+  if (err) {
+    console.log(err);
+  }else {
+    open('http://localhost:' + port);
+  }
 });
