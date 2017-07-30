5b6dbb27c72a7e848d563953f19c0678c69ef798
Jon Love <jonlov4@gmail.com>
2017-07-30 18:06:36 -0400

    Cache busting, seperate css, error tracking, bundle splitting, npm build script

---
+++ webpack.config.prod.js
@@ -0,0 +1,67 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[chunkhash].js'
+  },
+  plugins: [
+    // Generate external css file with its hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
+    // Append the hash of bundles to their file name to achieve cache busting
+    new WebpackMd5Hash(),
+
+    // Define separate bundles.
+    // This will compare files and leave duplicate references out of the main bundle.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+
+    // Create HTML file that includes reference to bundled JS
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      // Properties defined here are available in index.html
+      // htmlWebpackPlugin.options.prop
+      inject: true,
+      trackJSToken: 'd8fb3ffa4ea648529b9db1cd9f06b1bd'
+    }),
+
+    // Eliminate duplicate packages when generating bundle
+    new webpack.optimize.DedupePlugin(),
+
+    // Minify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
+    ]
+  }
+}
